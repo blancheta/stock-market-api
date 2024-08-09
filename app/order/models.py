1ec7faf3bdb74ffb181cbd341f859984cd4a3403
aldazar <alexandreblanchet44@gmail.com>
2024-08-09 12:29:01 +0100

    fix: view duplicate name

--- app/order/models.py
+++ app/order/models.py
@@ -1,13 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Column, Integer
-from sqlalchemy.orm import DeclarativeBase
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 
 
 class User(db.Model):
